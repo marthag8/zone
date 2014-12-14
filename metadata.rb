d66b1b62abb3f563f74acd1a895a9693cd1dfaf9
Martha Greenberg <marthag@techfinancials.com>
2014-12-14 20:46:27 +0200

    Added loopback filesystem support - pull request from matschaffer

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "marthag@mit.edu"
 license          "Apache 2.0"
 description      "Installs/Configures Solaris zones"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 supports "solaris2"
